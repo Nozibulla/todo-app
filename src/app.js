9f61c158f904b0844d5b8eb8d6e9b5294ea53923
Bashar Nozibulla <nbashar01@gmail.com>
2016-07-22 17:47:15 +0600

    add todos and remove todos

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
-[1,2,3].map(x => x * x)
-const PI = 3.1416;
-console.log(PI);
+(function($){
+
+	let appManager = {
+
+		
+		init : function(){
+
+			$('.container').on('submit', this.addTodo);
+
+			$('.container').on('click', '#delete',this.deleteTodo);
+
+		},
+
+		addTodo: e => {
+
+			e.preventDefault();
+
+			let newTodo = {name: $('#todo').val()};
+
+			let todoList = $('.todo-list');
+
+			let newTodoItem = `<li class="list-group-item clearfix" style=" margin-bottom: 8px;"> ${newTodo.name} <span class="pull-right button-group"><a href="#" style="margin-right: 8px;" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a><button type="button" id = "delete" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button></span></li>`;
+
+			todoList.append(newTodoItem);
+
+			$('#todo').val('');
+		},
+
+		deleteTodo: function(e) {
+
+			e.preventDefault();
+
+			$(this).parent().parent().remove();
+		}
+	};
+
+	$(function(){
+
+		appManager.init();
+
+	});
+
+})(jQuery);
